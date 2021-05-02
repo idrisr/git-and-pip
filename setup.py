9c76549e40bd5c62614e41ec1873dc9eedefbc27
Idris Raja <idris.raja@gmail.com>
2021-05-02 14:13:18 -0500

    update version

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gitandpip",
-    version="0.0.1",
-    author="arocketman",
-    author_email="and.capuano@gmail.com",
+    version="0.1.0",
+    author="idrisr",
+    author_email="idris.raja@gmail.com",
     description="It's pip... with git.",
     long_description=long_description,
-    url="https://github.com/arocketman/git-and-pip",
+    url="https://github.com/idrisr/git-and-pip",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
